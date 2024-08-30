311d8a527f01f33ac4b4a333a63fed8e1c287d0d
pelicanhere <fakegreenall@foxmail.com>
2024-08-30 14:25:06 +0800

    Update 09 HF.lean

--- myproject/09 HF.lean
+++ myproject/09 HF.lean
@@ -1,10 +1,8 @@
 import Mathlib
 
 open Polynomial
-
-
-/--Let $p$ be a prime number. Let $F$ be a field of characteristic $p$.
-Let $t \in F$ be an element which does not have a $p$th root in $F$.
-Then the polynomial $x^p - t$ is irreducible over $F$.-/
+/--Let `F` be a prime number. Let `F` be a field of characteristic `p`.
+Let `t ∈ F` be an element which does not have a `p` th root in `F`.
+Then the polynomial `x ^ p - t` is irreducible over $F$.[Stacks: Lemma 09HF, first part](https://stacks.math.columbia.edu/tag/09HF)-/
 theorem expand_irreducible {F : Type*} [Fact (Nat.Prime p)][Field F] {hp : 0 < p }{t : F} (p : ℕ )(ht : ∀ a : F, a ^ n ≠ t ) [CharP F p] :
  Irreducible (X ^ p - C t) := by sorry

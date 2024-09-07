af848561e31a91965ca3821df4c863648831d060
yuanyi-350 <kysyy1@126.com>
2024-09-07 09:27:31 +0800

    Update JacobsonNoether.lean
    Add names

--- Mathlib/FieldTheory/JacobsonNoether.lean
+++ Mathlib/FieldTheory/JacobsonNoether.lean
@@ -1,7 +1,8 @@
 /-
-Copyright (c) 2024 **ALL YOUR NAMES**Wanyi He, Filippo A. E. Nuccio, Huanyu Zheng. All rights reserved.
+Copyright (c) 2024 **ALL YOUR NAMES**Wanyi He, Filippo A. E. Nuccio, Huanyu Zheng, Weichen Jiao,
+ Yi Yuan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: **ALL YOUR NAMES** Wanyi He, Filippo A. E. Nuccio, Huanyu Zheng
+Authors: **ALL YOUR NAMES** Wanyi He, Filippo A. E. Nuccio, Huanyu Zheng, Weichen Jiao, Yi Yuan
 -/
 import Mathlib.RingTheory.Algebraic
 import Mathlib.FieldTheory.Separable
